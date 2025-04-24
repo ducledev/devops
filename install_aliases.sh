80bb486b628b30c6bb14892c075a0207f4441c91
Duc Le <ducle@DiMac>
2025-04-24 20:48:34 +0700

    update alias

--- install_aliases.sh
+++ install_aliases.sh
@@ -9,6 +9,10 @@ NEW_RC_PATH="$HOME/$f"
 
 cat > $NEW_RC_PATH << END_OF_FILE 
 
+# wsh wave terminal
+alias wv='wsh view'
+alias we='wsh edit'
+
 # Tmux aliases
 alias tmuxnew='tmux new -s'                     # tmuxnew session_name
 alias tmuxattach='tmux attach-session -t'       # tmuxattach session_name
@@ -26,6 +30,45 @@ TMUX_HELP_CONTENT=\$(cat <<EOM
 EOM
 )
 alias tmuxhelp='echo -e \$TMUX_HELP_CONTENT'
+
+# Kubernetes aliases
+alias k='kubectl'
+alias kgp='kubectl get pods'
+alias kgd='kubectl get deployments'
+alias kgs='kubectl get services'
+alias kgn='kubectl get nodes'
+alias kgns='kubectl get namespaces'
+alias kgall='kubectl get all'
+alias kdp='kubectl describe pod'
+alias kdd='kubectl describe deployment'
+alias kds='kubectl describe service'
+alias kl='kubectl logs'
+alias kex='kubectl exec -it'
+alias kns='kubectl config set-context --current --namespace'
+alias kctx='kubectl config use-context'
+alias kcurrent='kubectl config current-context'
+alias kctxs='kubectl config get-contexts'
+KUBE_HELP_CONTENT=\$(cat <<EOM
+ \n
+ k           = kubectl                                    \n
+ kgp         = kubectl get pods                           \n
+ kgd         = kubectl get deployments                    \n
+ kgs         = kubectl get services                       \n
+ kgn         = kubectl get nodes                          \n
+ kgns        = kubectl get namespaces                     \n
+ kgall       = kubectl get all                            \n
+ kdp [pod]   = kubectl describe pod                       \n
+ kdd [depl]  = kubectl describe deployment                \n
+ kds [svc]   = kubectl describe service                   \n
+ kl [pod]    = kubectl logs                               \n
+ kex [pod]   = kubectl exec -it                           \n
+ kns [ns]    = kubectl config set-context --current --namespace \n
+ kctx [ctx]  = kubectl config use-context                 \n
+ kcurrent    = kubectl config current-context             \n
+ kctxs       = kubectl config get-contexts                \n
+EOM
+)
+alias khelp='echo -e \$KUBE_HELP_CONTENT'
 
 # others
 alias cdssh='cd ~/.ssh'
